51825498575753ee4079d00b40eb59621a7fa73e
Zymed <miseso22@gmail.com>
2022-05-09 21:59:21 +0200

    Nothing fixed, still not writing anything within the Json file

--- EksamensProjekt/Assets/Sommer/Scripts/SaveToJson.cs
+++ EksamensProjekt/Assets/Sommer/Scripts/SaveToJson.cs
@@ -1,21 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class SaveToJson
 {
-    private string _filePath = @"C:\Unity stuff";
     private readonly string _fileName = "/timers.json";
+
     public void WriteTimersToJson()
     {
-        var outputString = JsonUtility.ToJson(TimerClass.Timers);
-        File.WriteAllText(Application.persistentDataPath + _fileName, outputString);
+        var jsonData = JsonUtility.ToJson(TimerClass.Timers);
+        File.WriteAllText(Application.persistentDataPath + _fileName, jsonData);
         Debug.Log("File has been saved as json file at: " + Application.persistentDataPath);
         Debug.Log("File has been saved with "+TimerClass.Timers.Count+" amount of numbers in the list.");
+        Debug.Log("The content of the list is: " + jsonData);
     }
 
-    public void ReadTimersToJson()
+    public void ReadTimersFromJson()
     {
         var inputString = File.ReadAllText(_fileName);
     }
--- EksamensProjekt/Assets/Sommer/Scripts/TimerClass.cs
+++ EksamensProjekt/Assets/Sommer/Scripts/TimerClass.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEngine;
 
+[Serializable]
 public class TimerClass
 {
     private readonly int _maxTimer = 60;
